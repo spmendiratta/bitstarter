230122fcf73f8bc7660143ad6fe98416e691ed2a
Satya Mendiratta <spmendiratta@yahoo.com>
2013-07-05 23:00:04 +0000

    Read index.html

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var content = String(fs.readFileSync("index.html"));
+  // response.send('Hello World 2!');
+   response.send( content );
 });
 
 var port = process.env.PORT || 5000;
