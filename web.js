796bdb915e5cbcc12e3f18662a88fcecaa075645
Satya Mendiratta <spmendiratta@yahoo.com>
2013-07-17 15:21:48 -0500

    minor change

--- web.js
+++ web.js
@@ -5,12 +5,12 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
   var content = String(fs.readFileSync("index.html"));
-  // response.send('Hello World !');
+  // response.send('Hello World!');
   // response.send('Hello World 2!');
    response.send( content );
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
